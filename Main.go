e34aed2560f808e5b847a37da74c5757dcf943f5
inakondu1 <inaisah66@gmail.com>
2026-01-24 12:50:15 +0100

    Refactor print statements and milestones in Main.go
    Updated print statements for clarity and corrected grammar.

--- Main.go
+++ Main.go
@@ -1,22 +1,30 @@
+// my first milestone
 package main
 
 import "fmt"
 
 func main() {
-	var fullName string = "Ina Hadiza Isah"
+	fmt.Println("Hello, World!")
+	// my second milestone
+
+	var fullName string = "Ina Hadiza Isah."
+	// my third milestone
 	var age int = 31
 	// let callculate how many years till i turn 50.
 	YearsToFifty := 50 - age
-	fmt.Println("Hello, World!")
-	fmt.Println("my name is", fullName)
-	fmt.Println("i am ", age, "years old and am learning go!")
-	fmt.Println("in the next ", YearsToFifty, " i will turn 50")
 
+	fmt.Println("my name is", fullName)
+	fmt.Println("I am ", age, "years old and I am learning go!")
+	fmt.Println("In the next ", YearsToFifty, "years I will turn 50.")
+	// my fouth milestone
 	// this will check if i am 30 or younger
 
 	if age >= 30 {
-		fmt.Println("i am old enough")
+		fmt.Println(" I am old enough to understand Golang.")
 	} else {
-		fmt.Println(" i am younger")
+		fmt.Println(" I am younger")
+
 	}
+
 }
+
