d5c8049bdf68e33293a1a3a2ee441c295e398b13
inakondu1 <inaisah66@gmail.com>
2026-01-24 12:16:38 +0100

    Refactor main function and add age check

--- Main.go
+++ Main.go
@@ -3,28 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World!")
-}
-
-milstone2
-package main
-import "fmt"
-func main() {	
-	var fullName string = "Ina Hadiza Isah"	
-	var age int  =  31	
-	fmt.Println("my name is",  fullName)	
-	fmt.Println("i am ", age, "years old")
-}
-
-        milestone 3
-package main
-import "fmt"func main() {	
 	var fullName string = "Ina Hadiza Isah"
-	var age int = 31	
-	// let callculate how many years till i turn 50.	
-	YearsToFifty := 50 - age	
-	fmt.Println("Hello, World!")	
+	var age int = 31
+	// let callculate how many years till i turn 50.
+	YearsToFifty := 50 - age
+	fmt.Println("Hello, World!")
 	fmt.Println("my name is", fullName)
-	fmt.Println("i am ", age, "years old and am learning go!")	
-	fmt.Println("in the next ", YearsToFifty, " i will turn 50")}
+	fmt.Println("i am ", age, "years old and am learning go!")
+	fmt.Println("in the next ", YearsToFifty, " i will turn 50")
 
+	// this will check if i am 30 or younger
+
+	if age >= 30 {
+		fmt.Println("i am old enough")
+	} else {
+		fmt.Println(" i am younger")
+	}
+}
