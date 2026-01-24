5eedfbe06827f3f20ec906062e016c801bdcc873
inakondu1 <inaisah66@gmail.com>
2026-01-24 11:37:30 +0100

    Update main function with personal information and milestone
    Refactor main function to include personal details and age calculation.

--- Main.go
+++ Main.go
@@ -6,22 +6,25 @@ func main() {
 	fmt.Println("Hello, World!")
 }
 
-milstone 2
-
+milstone2
 package main
-
 import "fmt"
-
-func main() {
-    // Milestone 1: The Entry Point
-    fmt.Println("Hello, World!")
-
-    // Milestone 2: Variables & Identity
-    // i used a string for my name and an integer for my age
-    var fullName string = "Ina Hadiza Isah"
-    var age int = 31
-
-    // Printing the variables using formatting
-    fmt.Printf("My name is" fullName)
-    fmt.Printf("I am",  age, "years old")
+func main() {	
+	var fullName string = "Ina Hadiza Isah"	
+	var age int  =  31	
+	fmt.Println("my name is",  fullName)	
+	fmt.Println("i am ", age, "years old")
 }
+
+        milestone 3
+package main
+import "fmt"func main() {	
+	var fullName string = "Ina Hadiza Isah"
+	var age int = 31	
+	// let callculate how many years till i turn 50.	
+	YearsToFifty := 50 - age	
+	fmt.Println("Hello, World!")	
+	fmt.Println("my name is", fullName)
+	fmt.Println("i am ", age, "years old and am learning go!")	
+	fmt.Println("in the next ", YearsToFifty, " i will turn 50")}
+
