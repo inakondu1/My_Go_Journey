e84fb25a4249f4690226b36ad57b902acbd1bb08
inakondu1 <inaisah66@gmail.com>
2026-01-24 09:47:24 +0100

    Implement variables and formatted output in main.go
    Added variables for name and age, and updated print statements.

---
+++ Main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+}
+
+milstone 2
+
+package main
+
+import "fmt"
+
+func main() {
+    // Milestone 1: The Entry Point
+    fmt.Println("Hello, World!")
+
+    // Milestone 2: Variables & Identity
+    // i used a string for my name and an integer for my age
+    var fullName string = "Ina Hadiza Isah"
+    var age int = 31
+
+    // Printing the variables using formatting
+    fmt.Printf("My name is" fullName)
+    fmt.Printf("I am",  age, "years old")
+}
